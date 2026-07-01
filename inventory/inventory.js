4636d23668047613c92938b3b48a1cc5d4822720
AeMyung <gwg16383@gmail.com>
2026-07-01 14:57:22 +0900

    dd
    dd

--- inventory/inventory.js
+++ inventory/inventory.js
@@ -1,3 +1,31 @@
+const sb = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+(async () => {
+
+    const memberCode = localStorage.getItem("member_code");
+
+    if (!memberCode) {
+        location.href = "../login/login.html";
+        return;
+    }
+
+    const { data } = await sb
+        .from("players")
+        .select("member_code")
+        .eq("member_code", memberCode)
+        .single();
+
+    if (!data) {
+        localStorage.removeItem("member_code");
+        location.href = "../login/login.html";
+        return;
+    }
+
+})();
+
 const list =
     document.getElementById(
         "inventoryList"
